ceed896ab52deccfcf5f43d30572d4f5a02b4887
podipodi909090 <163422328+podipodi909090@users.noreply.github.com>
2024-05-10 16:11:21 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -25,7 +25,7 @@ global.mess = {
 global.owner = [process.env.OWNER_NUMBER] || ['94711453097'] // Owner number , (අයිතිකරුගේ නම්බර් එක )
 
 module.exports = {    
-SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD:ObtainMessiah,
+SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD:CharlieBellhop,
 POSTGRESQL_URL: process.env.POSTGRESQL_URL || 'postgres://izumimd_meje_user:0Vhm5vKGZ7ORt2FlJBQf4d6EtRdeuE8z@dpg-cn0o2imn7f5s73fa46q0-a.frankfurt-postgres.render.com/izumimd_meje',
 PREFIX:  process.env.PREFIX || ['.'] ,
 LANG:  process.env.LANG || 'SI' ,    
